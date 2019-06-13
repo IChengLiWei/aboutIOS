bf0f994fd46ee4481bcdbc044eb6c00fcd2e5a12
IChengLiWei <869474792@qq.com>
2019-06-13 18:50:08 +0800

    阴影与圆角

--- testCopy/testCopy/copy/ViewController.m
+++ testCopy/testCopy/copy/ViewController.m
@@ -9,6 +9,7 @@
 #import "ViewController.h"
 #import "NewsDetailViewController.h"
 #import "Binar search.h"
+#import <Masonry.h>
 @interface ViewController ()
 @property(strong, nonatomic)NSString *strongStr;
 @property(copy, nonatomic)NSString *cStr;
@@ -32,7 +33,7 @@
 
 
 
-/*
+/*什么时候发生深copy,什么时候发生浅copy
  1. copy出来的字符串一定是不可变字符串，如果传入的是可变字符串，会发生深拷贝为不可变字符串，否则为浅拷贝(传入不可变字符串)。
  2. mutablecopy，一定是深拷贝，拷贝出来的一定是可变字符串或者数组，即使传入的是不可变字符串或者数组。
  3. NSString的原因在于使改字符串不可变
@@ -40,20 +41,47 @@
  */
 - (void)viewDidLoad {
     [super viewDidLoad];
-    [self GCDtest];
+//    [self CopyTest];
+//    [self GCDtest];
+    UIView *newView=[[UIView alloc] init];
+    newView.backgroundColor=[UIColor redColor];
+  
+    [self.view addSubview:newView];
+    [newView mas_makeConstraints:^(MASConstraintMaker *make) {
+        make.centerX.centerY.equalTo(self.view);
+        make.height.width.equalTo(@200);
+    }];
+     [newView.superview layoutIfNeeded];
+    newView.layer.cornerRadius=100;
+    
+    CALayer *shadowLayer      = [CALayer layer];
+    shadowLayer.backgroundColor=[UIColor purpleColor].CGColor;
+    shadowLayer.shadowColor   = [UIColor blackColor].CGColor;
+    shadowLayer.shadowOffset  = CGSizeMake(0, 0);
+    shadowLayer.shadowOpacity = 1;
+    shadowLayer.shadowRadius  = 10;
+    shadowLayer.frame         = newView.bounds;
+    [newView.layer addSublayer:shadowLayer];
    
+  
+    
+    
+    
+    
+    
   
     // Do any additional setup after loading the view, typically from a nib.
 }
 -(void)CopyTest{
     NSMutableString *mutStr=[[NSMutableString alloc] initWithFormat:@"啦啦啦"];
     self.strongStr=mutStr;
-    self.cStr=mutStr;
-    self.mStr=mutStr;
-    [self.mStr appendString:@"error"];
-    [mutStr appendString:@"gogogo"];
-    NSLog(@"%@,---%p---%p",self.strongStr,self.strongStr,mutStr);
-    NSLog(@"%@,---%p---%p",self.cStr,self.cStr,mutStr);
+    mutStr=nil;
+
+  
+    
+    
+    NSLog(@"%@,---指向对象的内存%p---指向对象的内存%p",self.strongStr,self.strongStr,mutStr);
+ 
 }
 -(void)GCDtest{
     
